e9b6fc29c4db77d8152e2e9656eb827296ce1e5f
Gerardo Quiñonez <gerardo01302011@gmail.com>
2020-02-14 19:04:47 -0600

    Ultimos cambios 3

--- js/crud.js
+++ js/crud.js
@@ -1,5 +1,4 @@
 var Fila = null
-
 function onSubmit() {
         let DataForm = Leer()
         if (Fila == null){
@@ -9,7 +8,6 @@ function onSubmit() {
             Vaciar()
     }
 }
-
 function Leer() {
     let DataForm = {}
     DataForm["nom"] = document.getElementById("nom").value
@@ -17,26 +15,23 @@ function Leer() {
     DataForm["pais"] = document.getElementById("pais").value
     return DataForm
 }
-
 function InsertarDatos(data) {
     let table = document.getElementById("tabla").getElementsByTagName('tbody')[0]
     let Fila = table.insertRow(table.length)
-    cell1 = Fila.insertCell(0).innerHTML = data.nom
-    cell2 = Fila.insertCell(1).innerHTML = data.ape
-    cell3 = Fila.insertCell(2).innerHTML = data.pais
-    cell3 = Fila.insertCell(3).innerHTML = `<input class="submit" type="button" onClick="Editarr(this)" value="Editar" >
+    columna1 = Fila.insertCell(0).innerHTML = data.nom
+    columna2 = Fila.insertCell(1).innerHTML = data.ape
+    columna3 = Fila.insertCell(2).innerHTML = data.pais
+    columna3 = Fila.insertCell(3).innerHTML = `<input class="submit" type="button" onClick="Editarr(this)" value="Editar" >
                                             <input class="submit" type="button" onClick="Borrarr(this)" value="Borrar" >`
     document.getElementById("nom").focus()
     Vaciar()
 }
-
 function Vaciar() {
     document.getElementById("nom").value = ""
     document.getElementById("ape").value = ""
     document.getElementById("pais").value = ""
     Fila = null
 }
-
 function Editarr(td) {
     Fila = td.parentElement.parentElement
     document.getElementById("nom").value = Fila.cells[0].innerHTML
@@ -49,7 +44,6 @@ function Actualizar(DataForm) {
     Fila.cells[2].innerHTML = DataForm.pais
     document.getElementById("nom").focus()
 }
-
 function Borrarr(td) {
     if (confirm('¿Seguro de borrar este registro?')) {
         row = td.parentElement.parentElement
