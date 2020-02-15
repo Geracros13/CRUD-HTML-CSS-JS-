2880603c116e6091a079e662ba0350794076710e
Gerardo Quiñonez <gerardo01302011@gmail.com>
2020-02-14 18:51:08 -0600

    Ultimos cambios

--- js/crud.js
+++ js/crud.js
@@ -1,78 +1,59 @@
+var Fila = null
 
-//Leer los valores y crear un array con ellos
-
-var data = Leer();
-
-function Leer(){   
-  var data = {};
-  data["nom"] = document.getElementById("nom").value;
-  data["ape"] = document.getElementById("ape").value;
-  data["pais"] = document.getElementById("pais").value;
-
-  return data;
+function onSubmit() {
+        let DataForm = Leer()
+        if (Fila == null){
+            InsertarDatos(DataForm)
+        } else{
+            Actualizar(DataForm)
+            Vaciar()
+    }
 }
 
-//Ir a la tabla e insertar columnas y filas en el body de la tabla
-
-function Insertar_datos_a_tabla(datos){
-  let table = document.getElementById('tabla').getElementsByTagName('tbody')[0];
-  let fila = table.insertRow(table.length);
-  fila.insertCell(0).innerHTML = datos.nom;
-  fila.insertCell(1).innerHTML = datos.ape;
-  fila.insertCell(2).innerHTML = datos.pais;
-  fila.insertCell(3).innerHTML = `<input class="submit" type="button" value="Editar" onclick="Editar(this)">
-                                  <input class="submit" type="button" value="Borrar" onclick="Borrar(this)">`;
-  document.getElementById("nom").focus();
+function Leer() {
+    let DataForm = {}
+    DataForm["nom"] = document.getElementById("nom").value
+    DataForm["ape"] = document.getElementById("ape").value
+    DataForm["pais"] = document.getElementById("pais").value
+    return DataForm;
 }
 
-
-function onFormSubmit() {
-  var datos = Leer();
-  Insertar_datos_a_tabla(datos);
-
+function InsertarDatos(data) {
+    let table = document.getElementById("tabla").getElementsByTagName('tbody')[0]
+    let Fila = table.insertRow(table.length)
+    cell1 = Fila.insertCell(0).innerHTML = data.nom
+    cell2 = Fila.insertCell(1).innerHTML = data.ape
+    cell3 = Fila.insertCell(2).innerHTML = data.pais
+    cell3 = Fila.insertCell(3).innerHTML = `<input class="submit" type="button" onClick="Editarr(this)" value="Editar" >
+                                            <input class="submit" type="button" onClick="Borrarr(this)" value="Borrar" >`
+    document.getElementById("nom").focus()
+    Vaciar()
 }
 
-
-
-//Vaciar los input despues de escribir los datos
-
-function Vaciar(){
-  document.getElementById("nom").value = '';
-  document.getElementById("ape").value = '';
-  document.getElementById("pais").value = '';
+function Vaciar() {
+    document.getElementById("nom").value = ""
+    document.getElementById("ape").value = ""
+    document.getElementById("pais").value = ""
+    Fila = null;
 }
 
-//Posicionarse en los valores para editarlos
-
-
-function Editar(columna){
-  filaActual = columna.parentElement.parentElement;
-  document.getElementById('nom').value = filaActual.cells[0].innerHTML;
-  document.getElementById('ape').value = filaActual.cells[1].innerHTML;
-  document.getElementById('pais').value = filaActual.cells[2].innerHTML;
-
+function Editarr(td) {
+    Fila = td.parentElement.parentElement
+    document.getElementById("nom").value = Fila.cells[0].innerHTML
+    document.getElementById("ape").value = Fila.cells[1].innerHTML
+    document.getElementById("pais").value = Fila.cells[2].innerHTML
+}
+function Actualizar(DataForm) {
+    Fila.cells[0].innerHTML = DataForm.nom
+    Fila.cells[1].innerHTML = DataForm.ape
+    Fila.cells[2].innerHTML = DataForm.pais
+    document.getElementById("nom").focus()
 }
 
-
-
-function Ed(){
-
-  data['nom'] = document.getElementById('nom').value;
-  data['ape'] = document.getElementById('ape').value;
-  data['pais'] = document.getElementById('pais').value;
-
-  Vaciar();
+function Borrarr(td) {
+    if (confirm('¿Seguro de borrar este registro?')) {
+        row = td.parentElement.parentElement
+        document.getElementById("tabla").deleteRow(row.rowIndex)
+        Vaciar()
+    }
 }
-
-
-
-
-
-function Borrar(columna) {
-  if (confirm('¿Desea borrar este registro?')) {
-      fila = columna.parentElement.parentElement;
-      document.getElementById('tabla').deleteRow(fila.rowIndex);
-      Vaciar();
-  }
-}
-
